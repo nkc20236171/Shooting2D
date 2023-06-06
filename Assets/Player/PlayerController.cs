200e3bee442b7dbdeaaaa18910dbf3609b220c25
nkc_suga <nkc23.suga.ryosei@gmail.com>
2023-06-06 15:12:01 +0900

    6/6

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 dir = Vector2.zero;     //ˆÚ“®•ûŒü‚ð•Û‘¶‚·‚é
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 5;
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += speed * Time.deltaTime * dir.normalized;
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -9f, 9f);
+        pos.y = Mathf.Clamp(pos.y, -5f, 5f);
+        transform.position = pos;
+
+    }
+}
